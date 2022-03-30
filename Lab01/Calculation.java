e556edf5f603c9a821b5f5bd4de421d082b4a3c8
guenhain163 <guenhain.163@gmail.com>
2022-03-30 21:56:21 +0700

    first program

---
+++ Lab01/Calculation.java
@@ -0,0 +1,27 @@
+import javax.swing.JOptionPane;
+
+public class Calculation {
+    public static void main(String[] args) {
+        String strNum1, strNum2;
+
+        strNum1 = JOptionPane.showInputDialog(null,
+                "Please input the first number: ", "Input the first number",
+                JOptionPane.INFORMATION_MESSAGE);
+
+        strNum2 = JOptionPane.showInputDialog(null,
+                "Please input the second number: ", "Input the second number",
+                JOptionPane.INFORMATION_MESSAGE);
+
+        double num1 = Double.parseDouble(strNum1);
+        double num2 = Double.parseDouble(strNum2);
+
+        JOptionPane.showMessageDialog(null,
+                "The sum of two numbers is: \t" + String.format("%.2f", (num1 + num2)) + "\n" +
+                "The difference of two numbers is: \t" + String.format("%.2f", (num1 - num2)) + "\n" +
+                "The product of two numbers is: \t" + String.format("%.2f", (num1 * num2)) + "\n" +
+                "The quotient  of two numbers is: \t" + String.format("%.2f", (num1 / num2)),
+                "Show result", JOptionPane.INFORMATION_MESSAGE);
+
+        System.exit(0);
+    }
+}
